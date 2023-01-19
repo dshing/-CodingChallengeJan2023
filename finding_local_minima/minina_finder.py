83fe4e3f326213b04f0b310075f57c0ebe75f4ca
David Shing <dvdshing@gmail.com>
2023-01-19 15:20:02 +0000

    fixed minima finder algorithm to more closely follow the requirements

--- finding_local_minima/minina_finder.py
+++ finding_local_minima/minina_finder.py
@@ -1,44 +1,55 @@
 import json
+from collections import OrderedDict
 
-# Basic iterate through all to find the lowest 
-# Very slow time complexity on large databases O(n)
-# Should opt for better algorithms like random access...
-# or others that reduce the amount of data point
+# find data points that fit f(t-1) > f(t) < f(t+1)
+# Assuming the first data point is also included because it matches f(t) < f(t+1)
+# or last datapoint matches f(t-1) > f(t)
 def method_A(set):
-    lowest = 0.0
-    key_idx = 0
-    first = True
+
+    minima_values = []
+
+    if (len(set) == 1):
+        return []
 
     for key, value in set.items():
-        if (first):
-            lowest = value
-            key_idx = key
-            first = False
-            # Add the first key value pair, because unordered dict ...
-            # I don't know if time = 1 is first
-        if (value < lowest):
-            lowest = value
-            key_idx = key
-    return [lowest, key_idx]
+        # deal with out of bounds keys
+        if (key == 1 and set[key + 1] > value): 
+            minima_values.append([value, key])
+        elif (key == len(set) and value < set[key - 1]):
+            minima_values.append([value, key])
+        # f(t-1) > f(t) < f(t+1)
+        elif (set[key - 1] > value and value < set[key + 1]):
+            minima_values.append([value, key])
+    
+    return minima_values
 
-# Assuming it is to detect the largest drop between two values in a dataset
+# Calculate e(t) = f(t)-f(t-1), check when there is a transition from e<0 to e>0
 def method_B(set):
-    lowest_change = 0
-    lowest_val = 0
-    key_lowest = 0
+    prev_change = 0
+
+    minima_values = []
+
+    if (len(set) == 1):
+        return []
+
     # want to compare 1 key value to another
     for key, value in set.items():
         if (key > 1):
-            prev_val = set[key - 1]
-            change = value - prev_val
-            if (change < lowest_change):
-                lowest_change = change
-                lowest_val = value
-                key_lowest = key
-    print(f"Lowest change is from {key_lowest} to {key_lowest - 1}: {lowest_change}")
-    return [lowest_val, key_lowest]
+            # first get the change value
+            change = sign_change_B(value, set[key - 1])
+            # compare change value with previous (e<0 to e>0)
+            if (prev_change < 0 and change > 0):
+                minima_values.append([value, key])
+            prev_change = change
+    return minima_values
 
-time_value = {}
+# e(t) = f(t)-f(t-1) function
+def sign_change_B(current_val, prev_val):
+    change = current_val - prev_val
+
+    return change    
+
+time_value = OrderedDict()
 # Json parse
 file = open("./finding_local_minima/data.json")
 data = json.load(file)
@@ -47,5 +58,6 @@ for item in data:
     time_value[item["time"]] = item["close"]
 file.close
 
-print(method_A(time_value))
-print(method_B(time_value))
+
+print(f"Method A local minima: {method_A(time_value)}")
+print(f"Method B local minima: {method_B(time_value)}")
